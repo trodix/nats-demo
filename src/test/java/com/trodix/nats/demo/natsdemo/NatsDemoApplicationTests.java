f4d464c171184a169179b9c042710f12be765d3a
Sebastien Vallet <sebastien.vallet89@gmail.com>
2024-08-10 16:31:24 +0200

    initial commit

---
+++ src/test/java/com/trodix/nats/demo/natsdemo/NatsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trodix.nats.demo.natsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
